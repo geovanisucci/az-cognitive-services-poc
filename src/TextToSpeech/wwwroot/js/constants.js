975e1b1f2a1f21e61bac35db5ba125b1c43c3c84
geovanis <geovanis@ciandt.com>
2022-10-13 15:23:13 -0300

    desenv

--- src/TextToSpeech/wwwroot/js/constants.js
+++ src/TextToSpeech/wwwroot/js/constants.js
@@ -1,1 +1,30 @@
 const SAUDACOES_LIZY = "Oi! Eu sou a LIA, a inteligência artificial do Lizy ERP. A partir de agora, é só falar Oi LIA que estou te ouvindo.";
+
+const POSSIBILIDADES_FRASE_OI_LIA = ["Oi, Lia.", "Oi Lia", "Oi, lia.", "Oi, lia"];
+
+const RESPOSTA_O_QUE_DESEJA_FAZER = "Oi, o que deseja fazer?";
+
+const RESPOSTA_NOTA_SERVICO_OU_VENDA = "Emitir nota de serviço ou de venda?";
+
+const RESPOSTA_EMISSAO_NFE_PEGUNTAR_NUMERO_VENDA = "Certo. E qual o número da venda?";
+
+const RESPOSTA_EMISSAO_NFE = "Ok. Só um instante enquanto emito a nota para você.";
+
+const RESPOSTA_NFE_EMITIDA = "A solicitação da emissão da Nota Fiscal de Venda foi concluída. Verifique no menu de Faturamento o status da emissão.";
+
+const POSSIBILIDADES_FRASE_EMITIR_NOTA = ["emitir nota", "emitir nota.", "emitir nota fiscal.", "emitir nota fiscal"];
+
+const POSSIBILIDADES_FRASE_EMITIR_NOTA_VENDA = ["emitir nota de venda.",
+                                                "emitir nota de venda", 
+                                                "emitir nota venda.",
+                                                "emitir nota venda",
+                                                "emitir nfe", 
+                                                "emitir nfe.", 
+                                                "emitir nota fiscal de venda",
+                                                "emitir nota fiscal de venda.",
+                                                "de venda",
+                                                "de venda.",
+                                                "di venda.",
+                                                "di venda",
+                                                "venda",
+                                                "venda.",];
--- src/TextToSpeech/wwwroot/js/lia.js
+++ src/TextToSpeech/wwwroot/js/lia.js
@@ -30,101 +30,94 @@ function saudacoesLizy(){
      falar(SAUDACOES_LIZY);
 }
 
-function conversar(){
-    var speechConfig = SpeechSDK.SpeechConfig.fromSubscription("54f558ca585747efa85d517e1c15bcbb", "brazilsouth");
-    speechConfig.speechSynthesisLanguage = "pt-BR"; 
-    speechConfig.speechSynthesisVoiceName = "pt-BR-FranciscaNeural";
-    recognizer = new SpeechSDK.IntentRecognizer(speechConfig);
-
-    //Adicionar intenções
-    //Oi Lia.
-    recognizer.addIntent("Oi, LIA.", "OiLIA");
-
-    recognizer.recognizeOnceAsync(
-      function (result) {
-        window.console.log(result);
-        switch (result.reason) {
-
-          case SpeechSDK.ResultReason.RecognizedSpeech:
-            falar("Oi, o que deseja fazer?");
-            break;
-
-          case SpeechSDK.ResultReason.RecognizedIntent:
-            falar("Oi, o que deseja fazer?");
-            break;
-
-          case SpeechSDK.ResultReason.NoMatch:
-            var noMatchDetail = SpeechSDK.NoMatchDetails.fromResult(result);
-            window.console.log(" NoMatchReason: " + SpeechSDK.NoMatchReason[noMatchDetail.reason]);
-            break;
-            
-          case SpeechSDK.ResultReason.Canceled:
-            var cancelDetails = SpeechSDK.CancellationDetails.fromResult(result);
-            window.console.log(" CancellationReason: " + SpeechSDK.CancellationReason[cancelDetails.reason]);
-            if (cancelDetails.reason === SpeechSDK.CancellationReason.Error) {
-              window.console.log(": " + cancelDetails.errorDetails);
-            }
-            break;
-        }
-      },
-      function (err) {
-        window.console.log(err);
-    });
-    
-}
-
-function iniciar(){
+function ouvir(){
 
     saudacoesLizy();
 
     var speechConfig = SpeechSDK.SpeechConfig.fromSubscription("54f558ca585747efa85d517e1c15bcbb", "brazilsouth");
     speechConfig.speechRecognitionLanguage = "pt-BR"; 
     var audioConfig  = SpeechSDK.AudioConfig.fromDefaultMicrophoneInput();
-    //recognizer = new SpeechSDK.SpeechRecognizer(speechConfig, audioConfig);
-    recognizer = new SpeechSDK.IntentRecognizer(speechConfig, audioConfig);
+    recognizer = new SpeechSDK.SpeechRecognizer(speechConfig, audioConfig);
+    //recognizer = new SpeechSDK.IntentRecognizer(speechConfig, audioConfig);
 
-    //Adicionar intenções
-     recognizer.addIntent("Oi Lia", "OiLia_I1");
-     recognizer.addIntent("Oi, Lia.", "OiLia_I2");
-     recognizer.addIntent("Oi, lia.", "OiLia_I3");
+    // //Adicionar intenções
+    //  recognizer.addIntent("Oi Lia", "OiLia_I1");
+    //  recognizer.addIntent("Oi, Lia.", "OiLia_I2");
+    //  recognizer.addIntent("Oi, lia.", "OiLia_I3");
+     let permiteFalar = false;
+     let emitirNFe = false;
 
     recognizer.startContinuousRecognitionAsync();
 
     recognizer.recognized = (reco, e) => {
 
+      if(isInArray(e.result.text, POSSIBILIDADES_FRASE_OI_LIA)){
+        falar(RESPOSTA_O_QUE_DESEJA_FAZER);
+        permiteFalar = true;
+        emitirNFe = false;
+      }
 
-        switch (e.result.reason) {
-
-            case SpeechSDK.ResultReason.RecognizedSpeech:
-                if(e.result.text == "Oi, Lia." || e.result.text == "Oi Lia" || e.result.text == "Oi, lia."){
-                    falar("Oi, o que deseja fazer?");
-                }
-                window.console.log(e.result.text);
-                break;
-    
-              case SpeechSDK.ResultReason.RecognizedIntent:
-                if(e.result.intentId == "OiLia_I1" || e.result.intentId == "OiLia_I2" || e.result.intentId == "OiLia_I3"){
-                    falar("Oi, o que deseja fazer?");
-                }
-                break;
-    
-              case SpeechSDK.ResultReason.NoMatch:
-                var noMatchDetail = SpeechSDK.NoMatchDetails.fromResult(result);
-                window.console.log(" NoMatchReason: " + SpeechSDK.NoMatchReason[noMatchDetail.reason]);
-                break;
-                
-              case SpeechSDK.ResultReason.Canceled:
-                var cancelDetails = SpeechSDK.CancellationDetails.fromResult(result);
-                window.console.log(" CancellationReason: " + SpeechSDK.CancellationReason[cancelDetails.reason]);
-                if (cancelDetails.reason === SpeechSDK.CancellationReason.Error) {
-                  window.console.log(": " + cancelDetails.errorDetails);
-                }
-                break;
+      //INTERACAO EMISSAO DE NOTA - INICIO
+      if(permiteFalar){
+        if(isInArray(e.result.text.toLowerCase(), POSSIBILIDADES_FRASE_EMITIR_NOTA)){
+          falar(RESPOSTA_NOTA_SERVICO_OU_VENDA)
         }
+        else if(isInArray(e.result.text.toLowerCase(), POSSIBILIDADES_FRASE_EMITIR_NOTA_VENDA)){
+          falar(RESPOSTA_EMISSAO_NFE_PEGUNTAR_NUMERO_VENDA);
+          emitirNFe = true;
+        }
+        else if(isNumeric(e.result.text)){
+          if(emitirNFe){
+            
+          falar(RESPOSTA_EMISSAO_NFE);
+
+          var delayInMilliseconds = 6000; //1 second
+
+          setTimeout(function() {
+          falar(RESPOSTA_NFE_EMITIDA);
+          emitirNFe = false;
+          permiteFalar = false;
+            //your code to be executed after 1 second
+          }, delayInMilliseconds);
+
+          }
+        }
+  
+      }
+
+        // switch (e.result.reason) {
+
+        //     case SpeechSDK.ResultReason.RecognizedSpeech:
+        //         if(e.result.text == "Oi, Lia." || e.result.text == "Oi Lia" || e.result.text == "Oi, lia."){
+        //             falar("Oi, o que deseja fazer?");
+        //         }
+        //         window.console.log(e.result.text);
+        //         break;
+    
+        //       case SpeechSDK.ResultReason.RecognizedIntent:
+        //         if(e.result.intentId == "OiLia_I1" || e.result.intentId == "OiLia_I2" || e.result.intentId == "OiLia_I3"){
+        //             falar("Oi, o que deseja fazer?");
+        //         }
+        //         break;
+    
+        //       case SpeechSDK.ResultReason.NoMatch:
+        //         var noMatchDetail = SpeechSDK.NoMatchDetails.fromResult(result);
+        //         window.console.log(" NoMatchReason: " + SpeechSDK.NoMatchReason[noMatchDetail.reason]);
+        //         break;
+                
+        //       case SpeechSDK.ResultReason.Canceled:
+        //         var cancelDetails = SpeechSDK.CancellationDetails.fromResult(result);
+        //         window.console.log(" CancellationReason: " + SpeechSDK.CancellationReason[cancelDetails.reason]);
+        //         if (cancelDetails.reason === SpeechSDK.CancellationReason.Error) {
+        //           window.console.log(": " + cancelDetails.errorDetails);
+        //         }
+        //         break;
+        // }
       };
 
       document.getElementById('stopSpeakTextAsyncButton').addEventListener("click", function () {
         recognizer.stopContinuousRecognitionAsync();
+        permiteFalar = false;
       });
     
 }
